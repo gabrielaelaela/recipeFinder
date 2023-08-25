cf0263742ff48009d7ce9bbaefd2f5bbfe3bfd2c
gabrielaelaela <ticugabriela12@gmail.com>
2023-08-25 15:21:37 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, FlatList, TextInput, Button, Alert, Image} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
+  const [keyword, setKeyword] = useState("");
+  const [result, setResult] = useState([]);
+
+  const find = () => {
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=" + keyword)
+        .then(response => response.json())
+        .then(data => setResult(data.meals))
+        .catch(error => {
+          Alert.alert('Error', error);
+        });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList style={{marginLeft : "5%", width: 300}} data={result} keyExtractor={(item, index) => index.toString()}
+                renderItem={({item}) =>
+                    <View>
+                      <Text style={{fontSize: 18, fontWeight: "bold", borderTopColor: "gray", borderTopWidth: 1}}>{item.strMeal}</Text>
+                      <Image style={{width:100, height: 100}}
+                             source={{uri: item.strMealThumb}} />
+
+                    </View>} />
+
+        <TextInput style={{fontSize: 15, width: 200, borderColor: 'gray', borderWidth: 1, textAlign: "center"}}
+                   placeholder="keyword" value={keyword} onChangeText={text => setKeyword(text)} />
+        <Button title={"Find"} onPress={find} />
     </View>
   );
 }
@@ -16,5 +39,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 15,
+    marginTop: 40
   },
 });
